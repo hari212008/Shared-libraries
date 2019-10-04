1bc959cc99dad541b5439bd23f535940109fa91d
hari212008 <hari212008@gmail.com>
2019-10-04 08:12:01 +0000

    variables

---
+++ vars/gittest.groovy
@@ -0,0 +1,28 @@
+import com.sapient.devops.scm
+
+def call(body)
+{
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+     stage('Cloning the code')
+     {
+       try {
+           def scm = new git()
+
+           scm.setValue("${config.BRANCH_NAME}")
+           scm.codeCheckout()
+           }
+       catch (Exception error)
+             {
+               wrap([$class: 'AnsiColorBuildWrapper']) {
+               echo "\u001B[41m[ERROR] ${error}"
+               def notify = new email()
+               notify.sendmail("$stage")
+               throw error
+                                                        }
+             }
+      }
+}
