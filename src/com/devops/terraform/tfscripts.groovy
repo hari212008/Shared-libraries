eb53e81031e82948dc245d625bfd0e8ab0f8d7c9
hari212008 <hari212008@gmail.com>
2019-10-07 17:04:59 +0000

    changes

--- src/com/devops/terraform/tfscripts.groovy
+++ src/com/devops/terraform/tfscripts.groovy
@@ -17,24 +17,3 @@ wrap([$class: 'AnsiColorBuildWrapper']) {
     }
   }
 }
-
-def plan()
-{
-wrap([$class: 'AnsiColorBuildWrapper']) {
-    try {
-      println "\u001B[32m [INFO] Starting TF plan. Please wait...\u001B[0m "
-     dir ('erv/') {
-      sh 'terraform get --update'
-      sh 'terraform plan '
-    }
-      println "\u001B[32m [INFO] \u001B[0m: TF initialization is \u001B[32m SUCCESSFULL\u001B[0m."
-    }
-    catch (Exception error) {
-      println "\u001B[41m [ERROR] failed to plan terraform."
-      throw error
-    }
-  }
-}
-
-
-
