a0190ed6eb0815fd6abfe4c40df78f88e4bc3b13
hari212008 <hari212008@gmail.com>
2019-10-07 07:12:31 +0000

    terrafrm checkout

---
+++ src/com/devops/scm/terraform.groovy
@@ -0,0 +1,35 @@
+package com.devops.scm
+
+void setValue(String brname)
+{
+   this.branch_name = brname
+}
+
+
+def terraformCheckout()
+{
+wrap([$class: 'AnsiColorBuildWrapper']) {
+    try {
+      this.branch_name = "$branch"
+      cleanWs()
+      println "\u001B[32m [INFO]Starting Terraform code checkout. Please wait...\u001B[0m "
+      checkout (
+        changelog: false,
+        poll: false,
+        scm :[
+        $class: 'GitSCM',
+        branches: [[name: "${branch_name}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [[url: 'https://github.com/hari212008/terrafrm.git']]
+        ]
+        )
+      println "\u001B[32m [INFO] \u001B[0m: Code checkout from \u001B[32m${branch_name}\u001B[0m is \u001B[32m SUCCESSFULL\u001B[0m."
+    }
+catch (Exception error) {
+      println "\u001B[41m [ERROR] failed to Checkout the tag $branch_name."
+      throw error
+    }
+  }
+}
---
+++ vars/terraformscm.groovy
@@ -0,0 +1,27 @@
+import com.devops.scm.terraform
+
+def call(body)
+{
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+     stage('Cloning the code')
+     {
+       try {
+           def terraform = new tf()
+         // ## println "branch name is ${BRANCH_NAME} "
+           scm.setValue("${config.BRANCH_NAME}")
+           scm.codeCheckout()
+           }
+       catch (Exception error)
+             {
+               wrap([$class: 'AnsiColorBuildWrapper']) {
+               echo "\u001B[41m[ERROR] ${error}"
+               throw error
+                                                        }
+             }
+      }
+}
+
