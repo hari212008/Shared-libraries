e1cb7663521e72c8f13fffec63c25580dbdb46a1
hari212008 <hari212008@gmail.com>
2019-10-09 12:42:58 +0000

    demo2

--- src/com/devops/terraform/tfscripts.groovy
+++ src/com/devops/terraform/tfscripts.groovy
@@ -14,7 +14,11 @@ wrap([$class: 'AnsiColorBuildWrapper']) {
     try {
       println "\u001B[32m [INFO] Starting TF action. Please wait...\u001B[0m "
       dir ('erv/'){
-        sh 'terraform "${tfaction}"'
+        sh 
+	'''
+	echo "action is $tfaction"
+	terraform "${tfaction}"
+	'''
 	    }
       println "\u001B[32m [INFO] \u001B[0m: TF initialization is \u001B[32m SUCCESSFULL\u001B[0m." + action
     }
