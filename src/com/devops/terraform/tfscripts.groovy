000d73d56f7b351daa51be36a6e0a023f3e51ba3
hari212008 <hari212008@gmail.com>
2019-10-09 11:31:59 +0000

    demo2

--- src/com/devops/terraform/tfscripts.groovy
+++ src/com/devops/terraform/tfscripts.groovy
@@ -9,7 +9,8 @@ wrap([$class: 'AnsiColorBuildWrapper']) {
         sh 'terraform --version'
         sh 'terraform init'
  	sh 'terraform plan'
-            }
+        sh 'terraform apply -auto-approve'
+	    }
       println "\u001B[32m [INFO] \u001B[0m: TF initialization is \u001B[32m SUCCESSFULL\u001B[0m."
     }
     catch (Exception error) {
