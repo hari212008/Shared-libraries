1cde15c8a25d5e425788b8b1e4a02572b83b73f0
hari212008 <hari212008@gmail.com>
2019-10-10 07:52:24 +0000

    demo2

--- src/com/devops/ecr_build/ecr.groovy
+++ src/com/devops/ecr_build/ecr.groovy
@@ -1,11 +1,12 @@
 package com.devops.ecr_build
 
-def push()
+def copying()
 {
 wrap([$class: 'AnsiColorBuildWrapper']) {
     try {
-        dir ('erv/src/Deployment/')
-	sh 'ls -lrth'
+      println "\u001B[32m [INFO] Copying EDA&Data processing scripts. Please wait...\u001B[0m "
+        sh 'sudo cp -R erv/ /jenkins/terraform/modules/usecase-setup/source'
+      println "\u001B[32m [INFO] \u001B[0m: Copying EDA&Data processing scripts is \u001B[32m SUCCESSFULL\u001B[0m."
     }
 catch (Exception error) {
       println "\u001B[41m [ERROR] failed to copying EDA&DATA."
@@ -13,6 +14,6 @@ catch (Exception error) {
     }
   }
 
-
 }
+
 
