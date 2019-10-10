007df35dd208194aa9da6198a2294002cb8b81f5
hari212008 <hari212008@gmail.com>
2019-10-10 09:20:30 +0000

    demo2

--- src/com/devops/ecr_build/ecr.groovy
+++ src/com/devops/ecr_build/ecr.groovy
@@ -14,7 +14,7 @@ wrap([$class: 'AnsiColorBuildWrapper']) {
 	//	dir('erv/src/Deployment/')
 		sh 'sudo ls -lrth erv/src/Deployment/'
 		echo "$imagename"			
-		sh 'bash erv/src/Deployment/build_and_push.sh "$imagename" v1'	
+		sh 'bash erv/src/Deployment/build_and_push.sh ecr v1'	
  // println "\u001B[32m [INFO] Copying EDA&Data processing scripts. Please wait...\u001B[0m "
       //  sh 'sudo cp -R erv/ /jenkins/terraform/modules/usecase-setup/source'
     //  println "\u001B[32m [INFO] \u001B[0m: Copying EDA&Data processing scripts is \u001B[32m SUCCESSFULL\u001B[0m."
