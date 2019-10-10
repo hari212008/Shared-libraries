6bdf8b84ca18c2cb481b46e96b80fc724b97bbf3
hari212008 <hari212008@gmail.com>
2019-10-10 07:31:28 +0000

    demo2

--- src/com/devops/ecr_build/ecr.groovy
+++ src/com/devops/ecr_build/ecr.groovy
@@ -1,24 +1,12 @@
 package com.devops.ecr_build
 
-//void setValue(String action)
-//{
-//this.imagename = image_name
-        //env.tfaction = action
-//}
-
-
 def push()
 {
 wrap([$class: 'AnsiColorBuildWrapper']) {
     try {
       println "\u001B[32m [INFO] Copying EDA&Data processing scripts. Please wait...\u001B[0m "
         dir ('erv/src/Deployment/')
-	echo '$image_name'
-        echo "$imagename"
 	sh 'ls -lrth'
-//	sh '$(aws ecr get-login)'
-//	sh 'bash build_and_push.sh churn v$BUILD_ID'
-//	sh 'bash build_and_push.sh churn v1' 
      println "\u001B[32m [INFO] \u001B[0m: Copying EDA&Data processing scripts is \u001B[32m SUCCESSFULL\u001B[0m."
     }
 catch (Exception error) {
