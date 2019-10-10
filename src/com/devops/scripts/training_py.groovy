d6790e70741dadd73f193c1dfd613799114c83ac
hari212008 <hari212008@gmail.com>
2019-10-10 12:36:47 +0000

    2

--- src/com/devops/scripts/training_py.groovy
+++ src/com/devops/scripts/training_py.groovy
@@ -1,13 +1,20 @@
 package com.devops.scripts
-
-
+void setValue(String script, String path)
+{
+env.script_name = script
+this.dir_path = path
+}
+if path == "null"
+{
+this.dir_path = $WORKSPACE
+}
 def pyt()
 {
 wrap([$class: 'AnsiColorBuildWrapper']) {
     try {
                 println "\u001B[32m [INFO] Invoking traninig Job Please wait...\u001B[0m "
-                dir('erv/src/') {
-		sh label: '', script: 'sh python.sh'
+                dir('$dir_path') {
+		sh label: '', script: 'sh ${script_name}'
         }
     }
 catch (Exception error) {
