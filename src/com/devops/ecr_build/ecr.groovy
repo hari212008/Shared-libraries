e1ab76580d422ce2a553c738e3e4ff6b6514a0e8
hari212008 <hari212008@gmail.com>
2019-10-10 10:01:42 +0000

    demo2

--- src/com/devops/ecr_build/ecr.groovy
+++ src/com/devops/ecr_build/ecr.groovy
@@ -12,10 +12,10 @@ wrap([$class: 'AnsiColorBuildWrapper']) {
     try {
 		println "\u001B[32m [INFO] Copying Build and push processing scripts. Please wait...\u001B[0m "    
 	//	dir('erv/src/Deployment/')
-		sh 'ls -lrth /erv/src/Deployment/'
+		sh 'ls -lrth /erv/src/Deployment/'.execute()
 		echo "$imagename"
 		sh '$(aws ecr get-login)'			
-		sh 'bash erv/src/Deployment/build_and_push.sh ecr v$BUILD_ID'.execute()	
+		sh 'bash erv/src/Deployment/build_and_push.sh ecr v$BUILD_ID'
     }
 catch (Exception error) {
       println "\u001B[41m [ERROR] failed to copying EDA&DATA."
